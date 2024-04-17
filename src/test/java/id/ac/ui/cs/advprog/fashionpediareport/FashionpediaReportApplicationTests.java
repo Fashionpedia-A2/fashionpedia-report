36de321120cf75f7ea1a1c9ab7c7a6a3db465811
RafGhan <rgharditama@gmail.com>
2024-04-17 13:57:24 +0700

    Init

---
+++ src/test/java/id/ac/ui/cs/advprog/fashionpediareport/FashionpediaReportApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.fashionpediareport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionpediaReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
