ca65ff2042a375f24032c23d75cc478d9f991a89
RafGhan <rgharditama@gmail.com>
2024-04-17 20:51:19 +0700

    fix controller

--- src/main/java/id/ac/ui/cs/advprog/fashionpediareport/controller/ReportController.java
+++ src/main/java/id/ac/ui/cs/advprog/fashionpediareport/controller/ReportController.java
@@ -1,12 +1,12 @@
 package id.ac.ui.cs.advprog.fashionpediareport.controller;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping("")
-public class ReportController{
-    @RequestMapping("")
-    public String main(){
-        return new String("Hello world");
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReportController {
+    @GetMapping("/")
+    public String review(){
+        return "Hello World!";
     }
 }
