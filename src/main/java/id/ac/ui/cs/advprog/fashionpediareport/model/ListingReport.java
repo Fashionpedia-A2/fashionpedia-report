92b79e02bca48b99084079a2180b199302c684fb
RafGhan <rgharditama@gmail.com>
2024-05-24 00:17:01 +0700

    [GREEN] implement updated model

--- src/main/java/id/ac/ui/cs/advprog/fashionpediareport/model/ListingReport.java
+++ src/main/java/id/ac/ui/cs/advprog/fashionpediareport/model/ListingReport.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.fashionpediareport.model;
 
+import jakarta.persistence.*;
 import id.ac.ui.cs.advprog.fashionpediareport.enums.ReportStatus;
 import id.ac.ui.cs.advprog.fashionpediareport.status.ListingPendingState;
 import id.ac.ui.cs.advprog.fashionpediareport.status.ListingReportState;
@@ -7,26 +8,50 @@ import id.ac.ui.cs.advprog.fashionpediareport.status.ListingReportState;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.*;
 
 
 @Getter
 @Setter
+@Entity
+@Table(name = "listing_report")
 public class ListingReport{
-    UUID id;
+    @Id
+    String id;
+
+    @Column(name = "user_id", nullable = false)
+    private String userId;
+
+    @Column(name = "reported_listing_id", nullable = false)
     String reportedListingId;
+
+    @Column(nullable = false)
     String alasan;
+
+    @Column(name= "report_status",nullable = false)
     ReportStatus status;
+
+    @Column(nullable = false)
     LocalDate date;
+
+    @Transient
+    @JsonIgnore
     ListingReportState state;
     
-    public ListingReport(String reportedListingId, String alasan){
-        this.id = UUID.randomUUID();
+    public ListingReport(String userId, String reportedListingId, String alasan){
+        this.userId = userId;
+        this.id = UUID.randomUUID().toString();
         this.reportedListingId = reportedListingId;
         this.alasan = alasan;
         this.status = ReportStatus.PENDING;
         this.date = LocalDate.now();
         this.state = new ListingPendingState();
+    }
+
+    public ListingReport() {
+
     }
 
     public void approve(){
